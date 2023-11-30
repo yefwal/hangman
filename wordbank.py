003605b0536b13f83db92ade5076d2c9b39b9b5e
yefwal <145699390+yefwal@users.noreply.github.com>
2023-11-30 11:31:04 -0800

    hangman game file
    wordbank game file

---
+++ wordbank.py
@@ -0,0 +1,44 @@
+# Animals
+animals = [
+    "Lion", "Elephant", "Giraffe", "Tiger", "Dolphin",
+    "Bear", "Wolf", "Kangaroo", "Penguin", "Cheetah"
+]
+
+# Fruits
+fruits = [
+    "Apple", "Banana", "Orange", "Grape", "Pineapple",
+    "Watermelon", "Strawberry", "Mango", "Kiwi", "Peach"
+]
+
+# Countries
+countries = [
+    "France", "Japan", "Brazil", "Australia", "Canada",
+    "Egypt", "India", "Spain", "Germany", "Mexico"
+]
+
+# Professions
+professions = [
+    "Doctor", "Engineer", "Teacher", "Musician", "Chef",
+    "Architect", "Athlete", "Lawyer", "Astronaut", "Firefighter"
+]
+
+# Colors
+colors = [
+    "Red", "Blue", "Green", "Yellow", "Purple",
+    "Orange", "Pink", "Black", "White", "Brown"
+]
+
+# Technology
+technology = [
+    "Computer", "Smartphone", "Internet", "Software", "Algorithm",
+    "Database", "Network", "Artificial Intelligence", "Cybersecurity", "Virtual Reality"
+]
+
+genres = {
+    "Animals": animals,
+    "Fruits": fruits,
+    "Countries": countries,
+    "Professions": professions,
+    "Colors": colors,
+    "Technology": technology
+}
